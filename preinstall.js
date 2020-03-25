7041d4e10596f129a0a692178ae8394dd41017a8
Adrian Wysocki <dobrystuff77@gmail.com>
2020-03-25 23:34:11 +0100

    problem with google credentials

--- preinstall.js
+++ preinstall.js
@@ -1,9 +1,19 @@
 const fs = require("fs");
 
-fs.writeFile(
-    "process.env.GOOGLE_APPLICATION_CREDENTIALS",
-    process.env.GOOGLE_CONFIG,
-    err => {
-        console.log(err);
-    }
-);
+// fs.writeFile(
+//     "process.env.GOOGLE_APPLICATION_CREDENTIALS",
+//     process.env.GOOGLE_CONFIG,
+//     err => {
+//         console.log(err);
+//     }
+// );
+
+fs.writeFile("./ala.txt", process.env.GOOGLE_CONFIG, err => {
+    console.log(err);
+});
+
+process.env.alejaja = {
+    alejaja: "alejaja"
+};
+
+console.log("process.env.alejaja: ", process.env.alejaja);
