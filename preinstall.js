5f5b29983537ad82075ee9cc26d5bca51605cde0
Adrian Wysocki <dobrystuff77@gmail.com>
2020-03-25 23:00:58 +0100

    problem with google credentials

--- preinstall.js
+++ preinstall.js
@@ -6,3 +6,7 @@ console.log("in production log credentials: ", process.env.GOOGLE_CONFIG);
 fs.writeFile("./google-credentials.json", process.env.GOOGLE_CONFIG, err => {
     console.log(err);
 });
+
+const credentials = require("./google-credentials.json");
+
+console.log("credeentials");
