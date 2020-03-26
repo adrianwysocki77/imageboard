c7718ad45f546961c8a54a829b7e71f9659dd3bc
Adrian Wysocki <dobrystuff77@gmail.com>
2020-03-26 02:47:19 +0100

    problem with google credentials

--- preinstall.js
+++ preinstall.js
@@ -14,7 +14,9 @@ const fs = require("fs");
 
 const credentials = JSON.stringify(process.env.GOOGLE_CONFIG);
 
-fs.writeFile("google-credentials-heroku.json", credentials, function(err) {
+fs.writeFile("config/google-credentials-heroku.json", credentials, function(
+    err
+) {
     if (err) {
         console.log("An error occured while writing JSON Object to File.");
         return console.log(err);
@@ -23,29 +25,29 @@ fs.writeFile("google-credentials-heroku.json", credentials, function(err) {
     console.log("JSON file has been saved.");
 });
 
-// const credentials2 = require("./google-credentials-heroku.json");
-//
-// console.log(credentials2);
+const credentials2 = require("config/google-credentials-heroku.json");
+
+console.log(credentials2);
 
 // file system module to perform file operations
 
 // json data
-var jsonData =
-    '{"persons":[{"name":"John","city":"New York"},{"name":"Phil","city":"Ohio"}]}';
-
-// parse json
-var jsonObj = JSON.parse(jsonData);
-console.log(jsonObj);
-
-// stringify JSON Object
-var jsonContent = JSON.stringify(jsonObj);
-console.log(jsonContent);
-
-fs.writeFile("output.json", jsonContent, "utf8", function(err) {
-    if (err) {
-        console.log("An error occured while writing JSON Object to File.");
-        return console.log(err);
-    }
-
-    console.log("JSON file has been saved.");
-});
+// var jsonData =
+//     '{"persons":[{"name":"John","city":"New York"},{"name":"Phil","city":"Ohio"}]}';
+//
+// // parse json
+// var jsonObj = JSON.parse(jsonData);
+// console.log(jsonObj);
+//
+// // stringify JSON Object
+// var jsonContent = JSON.stringify(jsonObj);
+// console.log(jsonContent);
+//
+// fs.writeFile("output.json", jsonContent, "utf8", function(err) {
+//     if (err) {
+//         console.log("An error occured while writing JSON Object to File.");
+//         return console.log(err);
+//     }
+//
+//     console.log("JSON file has been saved.");
+// });
