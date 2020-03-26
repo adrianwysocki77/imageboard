829037b48c1c5c7bb2af323547fd1922835d905d
Adrian Wysocki <dobrystuff77@gmail.com>
2020-03-26 11:27:26 +0100

    problem with google credentials

--- preinstall.js
+++ preinstall.js
@@ -42,7 +42,7 @@ console.log(jsonObj);
 var jsonContent = JSON.stringify(jsonObj);
 console.log(jsonContent);
 
-fs.writeFile("output.json", jsonContent, "utf8", function(err) {
+fs.writeFile("./output.json", jsonContent, "utf8", function(err) {
     if (err) {
         console.log("An error occured while writing JSON Object to File.");
         return console.log(err);
