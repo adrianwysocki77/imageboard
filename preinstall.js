00c2bee4f4d61aedfaba9c56865666019cb9923f
Adrian Wysocki <dobrystuff77@gmail.com>
2020-03-26 16:25:05 +0100

    problem with google credentials

--- preinstall.js
+++ preinstall.js
@@ -33,7 +33,8 @@ fs.writeFile(
 
 setTimeout(function() {
     let credentials2 = require("./google-credentials-heroku.json");
-    console.log(credentials2);
+    console.log("credentials2", credentials2);
+    console.log("credentials2.client_email", credentials2.client_email);
 }, 2000);
 
 ////////////////////////////////////////////////////////////////////////////////
