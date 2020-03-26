043278a1847bcdf0fe5715bd8e19500dab43ef33
Adrian Wysocki <dobrystuff77@gmail.com>
2020-03-26 11:30:08 +0100

    problem with google credentials

--- preinstall.js
+++ preinstall.js
@@ -12,37 +12,9 @@ const fs = require("fs");
 //     console.log(err);
 // });
 ////////////////////////////////////////////////////////////////////////////////
-// const credentials = JSON.stringify(process.env.GOOGLE_CONFIG);
-//
-// fs.writeFile("google-credentials-heroku.json", credentials, function(err) {
-//     if (err) {
-//         console.log("An error occured while writing JSON Object to File.");
-//         return console.log(err);
-//     }
-//
-//     console.log("JSON file has been saved.");
-// });
-//
-// const credentials2 = require("config/google-credentials-heroku.json");
-//
-// console.log(credentials2);
-////////////////////////////////////////////////////////////////////////////////
+const credentials = JSON.stringify(process.env.GOOGLE_CONFIG);
 
-// file system module to perform file operations
-
-// json data
-var jsonData =
-    '{"persons":[{"name":"John","city":"New York"},{"name":"Phil","city":"Ohio"}]}';
-
-// parse json
-var jsonObj = JSON.parse(jsonData);
-console.log(jsonObj);
-
-// stringify JSON Object
-var jsonContent = JSON.stringify(jsonObj);
-console.log(jsonContent);
-
-fs.writeFile("./output.json", jsonContent, "utf8", function(err) {
+fs.writeFile("./google-credentials-heroku.json", credentials, function(err) {
     if (err) {
         console.log("An error occured while writing JSON Object to File.");
         return console.log(err);
@@ -51,5 +23,33 @@ fs.writeFile("./output.json", jsonContent, "utf8", function(err) {
     console.log("JSON file has been saved.");
 });
 
-let newFile = require("./output.json");
-console.log("newFile: ", newFile);
+const credentials2 = require("./config/google-credentials-heroku.json");
+
+console.log(credentials2);
+////////////////////////////////////////////////////////////////////////////////
+
+// // file system module to perform file operations
+//
+// // json data
+// var jsonData =
+//     '{"persons":[{"name":"John","city":"New York"},{"name":"Phil","city":"Ohio"}]}';
+//
+// // parse json
+// var jsonObj = JSON.parse(jsonData);
+// console.log(jsonObj);
+//
+// // stringify JSON Object
+// var jsonContent = JSON.stringify(jsonObj);
+// console.log(jsonContent);
+//
+// fs.writeFile("./output.json", jsonContent, "utf8", function(err) {
+//     if (err) {
+//         console.log("An error occured while writing JSON Object to File.");
+//         return console.log(err);
+//     }
+//
+//     console.log("JSON file has been saved.");
+// });
+//
+// let newFile = require("./output.json");
+// console.log("newFile: ", newFile);
